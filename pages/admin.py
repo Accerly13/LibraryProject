9dd41cc8aac9d1710ce2bbb11b943c096d63abb8
Kurt Anjelo M. Sereño <kusereno@gbox.adnu.edu.ph>
2023-01-26 11:28:20 +0800

    dsdsdsds

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Admins
 
 # Register your models here.
+
+class AdminsAdmin(admin.ModelAdmin):
+    list_display = ['admin_username', 'admin_password']
+
+admin.site.register(Admins, AdminsAdmin)
