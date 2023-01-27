af43bd8a02f2a7defdcf842b86f9008f6d09c626
Kurt Anjelo M. Sereño <kusereno@gbox.adnu.edu.ph>
2023-01-27 08:22:09 +0800

    potang ina mo reine

--- pages/urls.py
+++ pages/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
    path('admin/dashboard/updaterecord', views.UpdateRecord.as_view(), name='updaterecord'),
    path('admin/dashboard/deleterecord', views.DeleteRecord.as_view(), name='deleterecord'),
    path('admin/dashboard/managereport', views.ManageReport.as_view(), name='managereport'),
-   path('', views.HomePageViewStudent.as_view(), name='home'),
-   path('stat/', views.DashBoardStudent.as_view(), name='stat')
+   path('1/', views.HomePageViewStudent.as_view(), name='home'),
+   path('', views.DashBoardStudent.as_view(), name='stat')
 ]
