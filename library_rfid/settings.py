ae2e896174dbe5242f3bf4dc262f725792687dae
Kurt Anjelo M. Sereño <kusereno@gbox.adnu.edu.ph>
2023-04-25 12:50:50 +0800

    bug at add is fixed

--- library_rfid/settings.py
+++ library_rfid/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'library_database',  
         'USER': 'root',  
-        'PASSWORD': '',  
+        'PASSWORD': 'libraryrfid',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
